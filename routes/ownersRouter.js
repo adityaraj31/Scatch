e8f493362a913681f7f41c3e7279862f5d274b83
adityaraj31 <rajadityasingh3007@gmail.com>
2024-12-12 18:35:28 +0530

    code commented

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const router = express.Router();
-const ownerModel = require("../model/owner-model");
+// const express = require("express");
+// const router = express.Router();
+// const ownerModel = require("../model/owner-model");
 
-if (process.env.NODE_ENV == "development") {
-  router.post("/create", async (req, res) => {
-    let owners = await ownerModel.find();
-    if (owners.length > 0)
-      return res
-        .status(503)
-        .send("You don't have permission to create a new owner");
+// if (process.env.NODE_ENV == "development") {
+//   router.post("/create", async (req, res) => {
+//     let owners = await ownerModel.find();
+//     if (owners.length > 0)
+//       return res
+//         .status(503)
+//         .send("You don't have permission to create a new owner");
 
-    let { fullname, email, password } = req.body;
+//     let { fullname, email, password } = req.body;
 
-    let createdOwner = await ownerModel.create({
-      fullname,
-      email,
-      password,
-    });
-    res.status(201).send(createdOwner);
-  });
-}
+//     let createdOwner = await ownerModel.create({
+//       fullname,
+//       email,
+//       password,
+//     });
+//     res.status(201).send(createdOwner);
+//   });
+// }
 
-router.get("/admin", (req, res) => {
-  let success = req.flash("success");
-  res.render("createproducts", { success });
-});
+// router.get("/admin", (req, res) => {
+//   let success = req.flash("success");
+//   res.render("createproducts", { success });
+// });
 
-module.exports = router;
+// module.exports = router;
