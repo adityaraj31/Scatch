313930a8e01820973050c49aef9c57c457394f80
adityaraj31 <rajadityasingh3007@gmail.com>
2024-12-13 08:28:20 +0530

    some changes in routes

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,29 @@ const express = require("express");
 const router = express.Router();
 const isLoggedIn = require("../middlewares/isLoggedIn");
 const productModel = require("../model/product-model");
+const userModel = require("../model/user-model");
 
 router.get("/", (req, res) => {
   let error = req.flash("error");
-  res.render("index", { error });
+  res.render("index", { error, loggedin: false });
 });
 
 router.get("/shop", isLoggedIn, async function (req, res) {
-  let products = await productModel.find()
-  res.render("shop", {products});
+  let products = await productModel.find();
+  let success = req.flash("success");
+  res.render("shop", { products, success });
+});
+
+router.get("/cart", isLoggedIn, async (req, res) => {
+  res.render("cart");
+});
+
+router.get("/addtocart/:productid", isLoggedIn, async function (req, res) {
+  let user = await userModel.findOne({ email: req.user.email });
+  user.cart.push(req.params.productid);
+  user.save();
+  req.flash("success", "Added to cart");
+  res.redirect("/shop");
 });
 
 router.get("/logout", isLoggedIn, function (req, res) {
